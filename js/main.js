4807292b670508abcc971e34d707ed8b74156245
Maru Di Primio <mariana.diprimio@gmail.com>
2022-08-09 13:22:51 +0200

    menu desplegable,barra de scroll y goUp OK

--- js/main.js
+++ js/main.js
@@ -3,14 +3,20 @@ document.getElementById("menuIcon").addEventListener("click", aparece);
 
 function aparece() {
   document.getElementById("menu").classList.toggle('open');
-  console.log('hola')
 }
 
 let barra = document.querySelector("#scroll");
 window.onscroll = function captureScroll() {
-  console.log("Vertical: " + window.scrollY);
+  //console.log("Vertical: " + window.scrollY);
   let max = document.body.scrollHeight - innerHeight;
   barra.style.width = `${(window.scrollY / max) * 100}%`;
 };
 
 
+document.getElementById("upBTN").addEventListener("click", subir);
+
+function subir(){
+  setTimeout(function(){
+    window.scrollTo({top:0, behavior:'smooth'});
+  },200)
+}
